c1c8ab76459bfe1e78a615e6c11194b264f55e1a
JorgeGit14288 <jorge.fuentes.14288@gmail.com>
2018-03-03 13:03:43 -0600

    Se creo mostrar la segunda pantalla

--- src/pages/mostrar-capcha/mostrar-capcha.ts
+++ src/pages/mostrar-capcha/mostrar-capcha.ts
@@ -18,10 +18,38 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class MostrarCapchaPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.capcha =navParams.data.capcha;
+    this.base64Image = "data:image/jpeg;base64," + this.capcha.imagen;
+    console.log("LLego capcha", this.capcha);
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MostrarCapchaPage');
+  }
+  public capcha: any = {
+    codigo: "",
+    imagen: "",
+    transaccion: ""
+  };
+
+  public respuestaCapcha ={
+    transaccion: "",
+    codigoCapcha: "",
+    codigoSistema:"",
+  }
+
+  public codigoCap ="";
+  base64Image = "";
+
+  onSubmit():void{
+    this.respuestaCapcha.transaccion = this.capcha.transaccion;
+    this.respuestaCapcha.codigoSistema="cs1";
+    this.codigoCap = this.codigoCap;
+
+
+
+    console.log("Enviando datos ")
+
   }
 
 }
